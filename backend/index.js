bd7d137823c12920130e8cc71db3bf9c6563617e
VoinzzZ <antonynugroho467@gmail.com>
2025-09-09 09:46:27 +0700

    fix: routes api

--- backend/index.js
+++ backend/index.js
@@ -1,31 +1,9 @@
 require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const connectDB = require("./app/config/db.mongo");
-
-const authRoutes = require("./app/routes/auth.routes");
-const noteRoutes = require("./app/routes/note.route");
-
-const app = express();
-
-// Middleware
-app.use(cors());
-app.use(express.json());
-
-// DB Connect
-connectDB();
-
-// Routes
-app.use("/api/auth", authRoutes);
-app.use("/api/notes", noteRoutes);
-
-// Default route
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
+const app = require("./app/app")
 
 // Server listen
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on port ${PORT}`);
 });
