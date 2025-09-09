2aadc4ed5728c39ac8af3355774c6ca2c8e132d1
VoinzzZ <antonynugroho467@gmail.com>
2025-09-09 09:46:46 +0700

    add: route app

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,14 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-const morgan = require("morgan");
-const authRoutes = require("./routes/authRoutes");
+const connectDB = require("./app/config/db.mongo");
+
+const authRoutes = require("./app/routes/auth.routes");
+const noteRoutes = require("./app/routes/note.route");
 
 const app = express();
 
+// Middleware
 app.use(cors());
 app.use(express.json());
-app.use(morgan("dev"));
 
+// DB Connect
+connectDB();
+
+// Routes
 app.use("/api/auth", authRoutes);
+app.use("/api/notes", noteRoutes);
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
 
 module.exports = app;
